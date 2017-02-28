8ba4b180b845abb1ed59b104004e1b9439637270
Revln9 <sami.chetbi@gmail.com>
2017-02-28 11:08:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
-import './styles/app.scss'
+import './styles/app.css'
 
 render(<App/>, document.getElementById('main'))
